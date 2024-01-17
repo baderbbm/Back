33effd799bf194ef42b41ded9447ccc6eb1695ce
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-01-17 17:00:22 +0100

    Update PatientNotFoundException.java

--- src/main/java/com/microservices/exceptions/PatientNotFoundException.java
+++ src/main/java/com/microservices/exceptions/PatientNotFoundException.java
@@ -1,1 +1,8 @@
+package com.microservices.exceptions;
 
+public class PatientNotFoundException extends RuntimeException {
+
+    public PatientNotFoundException(String message) {
+        super(message);
+    }
+}
