269a15d906df3b2806e4ef6e4419080ebf16dd67
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-01-17 20:19:38 +0100

    Update PatientService.java

--- src/main/java/com/microservices/services/PatientService.java
+++ src/main/java/com/microservices/services/PatientService.java
@@ -1,6 +1,7 @@
 package com.microservices.services;
 
 import com.microservices.domain.Patient;
+import com.microservices.exceptions.PatientNotFoundException;
 import com.microservices.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,27 @@ public class PatientService {
 
     public Optional<Patient> getPatientById(Long id) {
         return patientRepository.findById(id);
+    }
+    
+    public Patient updatePatientAdresse(Long id, String nouvelleAdresse) {
+        Optional<Patient> optionalPatient = patientRepository.findById(id);
+        if (optionalPatient.isPresent()) {
+            Patient patient = optionalPatient.get();
+            patient.setAdressePostale(nouvelleAdresse);
+            return patientRepository.save(patient);
+        } else {
+            throw new PatientNotFoundException("Patient non trouvé avec l'ID : " + id);
+        }
+    }
+
+    public Patient updatePatientNumero(Long id, String nouveauNumero) {
+        Optional<Patient> optionalPatient = patientRepository.findById(id);
+        if (optionalPatient.isPresent()) {
+            Patient patient = optionalPatient.get();
+            patient.setNumeroTelephone(nouveauNumero);
+            return patientRepository.save(patient);
+        } else {
+            throw new PatientNotFoundException("Patient non trouvé avec l'ID : " + id);
+        }
     }
 }
