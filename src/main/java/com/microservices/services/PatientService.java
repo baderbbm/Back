8dd057625626f17a208514d5f40f769e48020b82
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-01-18 21:00:21 +0100

    Update PatientService.java

--- src/main/java/com/microservices/services/PatientService.java
+++ src/main/java/com/microservices/services/PatientService.java
@@ -1,5 +1,6 @@
 package com.microservices.services;
 
+import com.microservices.domain.Genre;
 import com.microservices.domain.Patient;
 import com.microservices.exceptions.PatientNotFoundException;
 import com.microservices.repositories.PatientRepository;
@@ -12,36 +13,49 @@ import java.util.Optional;
 @Service
 public class PatientService {
 
-    @Autowired
-    private PatientRepository patientRepository;
+	@Autowired
+	private PatientRepository patientRepository;
 
-    public List<Patient> getAllPatients() {
-        return patientRepository.findAll();
-    }
+	public List<Patient> getAllPatients() {
+		return patientRepository.findAll();
+	}
 
-    public Optional<Patient> getPatientById(Long id) {
-        return patientRepository.findById(id);
-    }
-    
-    public Patient updatePatientAdresse(Long id, String nouvelleAdresse) {
-        Optional<Patient> optionalPatient = patientRepository.findById(id);
-        if (optionalPatient.isPresent()) {
-            Patient patient = optionalPatient.get();
-            patient.setAdressePostale(nouvelleAdresse);
-            return patientRepository.save(patient);
-        } else {
-            throw new PatientNotFoundException("Patient non trouvé avec l'ID : " + id);
-        }
-    }
+	public Optional<Patient> getPatientById(Long id) {
+		return patientRepository.findById(id);
+	}
 
-    public Patient updatePatientNumero(Long id, String nouveauNumero) {
-        Optional<Patient> optionalPatient = patientRepository.findById(id);
-        if (optionalPatient.isPresent()) {
-            Patient patient = optionalPatient.get();
-            patient.setNumeroTelephone(nouveauNumero);
-            return patientRepository.save(patient);
-        } else {
-            throw new PatientNotFoundException("Patient non trouvé avec l'ID : " + id);
-        }
-    }
+	public Patient updatePatientAdresse(Long id, String nouvelleAdresse) {
+		Optional<Patient> optionalPatient = patientRepository.findById(id);
+		if (optionalPatient.isPresent()) {
+			Patient patient = optionalPatient.get();
+			patient.setAdressePostale(nouvelleAdresse);
+			return patientRepository.save(patient);
+		} else {
+			throw new PatientNotFoundException("Patient non trouvé avec l'ID : " + id);
+		}
+	}
+
+	public Patient updatePatientNumero(Long id, String nouveauNumero) {
+		Optional<Patient> optionalPatient = patientRepository.findById(id);
+		if (optionalPatient.isPresent()) {
+			Patient patient = optionalPatient.get();
+			patient.setNumeroTelephone(nouveauNumero);
+			return patientRepository.save(patient);
+		} else {
+			throw new PatientNotFoundException("Patient non trouvé avec l'ID : " + id);
+		}
+	}
+
+	public Patient addNewPatient(String prenom, String nom, java.sql.Date dateNaissance, Genre genre,
+			String adressePostale, String numeroTelephone) {
+		Patient newPatient = new Patient();
+		newPatient.setPrenom(prenom);
+		newPatient.setNom(nom);
+		newPatient.setDateNaissance(dateNaissance);
+		newPatient.setGenre(genre);
+		newPatient.setAdressePostale(adressePostale);
+		newPatient.setNumeroTelephone(numeroTelephone);
+
+		return patientRepository.save(newPatient);
+	}
 }
