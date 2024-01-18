31fb95da302daf2ee3605c52b41c227df1027364
baderbbm <131297523+baderbbm@users.noreply.github.com>
2024-01-18 20:59:51 +0100

    Update PatientController.java

--- src/main/java/com/microservices/controllers/PatientController.java
+++ src/main/java/com/microservices/controllers/PatientController.java
@@ -4,10 +4,13 @@ import com.microservices.domain.Patient;
 import com.microservices.exceptions.PatientNotFoundException;
 import com.microservices.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,6 +65,23 @@ public class PatientController {
             return ResponseEntity.ok(patient);
         } catch (PatientNotFoundException e) {
             return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PostMapping("/add")
+    public ResponseEntity<Patient> addNewPatient(@RequestBody Patient newPatient) {
+        try {
+            Patient addedPatient = patientService.addNewPatient(
+                    newPatient.getPrenom(),
+                    newPatient.getNom(),
+                    newPatient.getDateNaissance(),
+                    newPatient.getGenre(),
+                    newPatient.getAdressePostale(),
+                    newPatient.getNumeroTelephone()
+            );
+            return ResponseEntity.ok(addedPatient);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
